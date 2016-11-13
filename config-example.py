f65412094c7cc10ce1cb7f17943615a7a28d62d0
Clément Rigo <rigoclement@mydnic.be>
2016-11-13 22:50:46 +0100

    Testing better output

--- config-example.py
+++ config-example.py
@@ -2,11 +2,12 @@
 # Rename this file "config.py" and you're done !
 
 host = "DEVICE_LOCAL_IP"
-port = 'PORT_NUMBER'
+port = 22
 username = "root"
 password = "password"
 
 filePath = '/storage/emulated/0/Music'
-fileExtension = '"*.mp3"'
+fileExtension = '.mp3'
+filePattern = '"*.mp3"'
 
 localPath = '/home/pi/NAS/Music'
--- execute.py
+++ execute.py
@@ -3,14 +3,17 @@ from config import *
 
 # Prepare Command
 rawcommand = 'find {path} -name {pattern}'
-command = rawcommand.format(path=filePath, pattern=fileExtension)
+command = rawcommand.format(path=filePath, pattern=filePattern)
 
 # Connect to Android Device
+print 'Start Connexion...'
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host, port=port, username=username, password=password)
+print 'Connected.'
 
 # List the distant files and chose a random one
+print 'Listing all files from distant device...'
 distantFiles = list()
 
 stdin, stdout, stderr = ssh.exec_command(command)
@@ -22,19 +25,23 @@ for afile in filelist:
     distantFiles.append(filename)
 
 chosenDistantFile = random.choice(distantFiles)
+print chosenDistantFile, ' is the distant file that was randomly picked'
 
 # List the local files and chose a random one
+print 'Listing all files locally'
 localFiles = list()
 
-def mp3gen():
+def fileLoop():
     for root, dirs, files in os.walk(localPath):
         for filename in files:
             if os.path.splitext(filename)[1] == fileExtension:
                 yield os.path.join(root, filename)
 
-for mp3file in mp3gen():
-    localFiles.append(mp3file)
+for file in fileLoop():
+    localFiles.append(file)
 
 chosenLocalFile = random.choice(localFiles)
+
+print chosenLocalFile, ' is the local file that was randomly picked'
 
 print 'we will delete', chosenDistantFile, ' and replace with', chosenLocalFile
