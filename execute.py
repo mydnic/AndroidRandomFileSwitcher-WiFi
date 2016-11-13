9174073e173a3f827b19376243d529d9948c9036
Clément Rigo <rigoclement@mydnic.be>
2016-11-13 23:23:42 +0100

    Delete and transfer the files

--- execute.py
+++ execute.py
@@ -10,6 +10,7 @@ print 'Start Connexion...'
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host, port=port, username=username, password=password)
+sftp = ssh.open_sftp()
 print 'Connected.'
 
 # List the distant files and chose a random one
@@ -19,16 +20,15 @@ distantFiles = list()
 stdin, stdout, stderr = ssh.exec_command(command)
 filelist = stdout.read().splitlines()
 
-ftp = ssh.open_sftp()
 for afile in filelist:
     (head, filename) = os.path.split(afile)
     distantFiles.append(filename)
 
 chosenDistantFile = random.choice(distantFiles)
-print chosenDistantFile, ' is the distant file that was randomly picked'
+print chosenDistantFile, 'is the distant file that was randomly picked'
 
 # List the local files and chose a random one
-print 'Listing all files locally'
+print 'Listing all local files...'
 localFiles = list()
 
 def fileLoop():
@@ -41,7 +41,19 @@ for file in fileLoop():
     localFiles.append(file)
 
 chosenLocalFile = random.choice(localFiles)
+(chosenPath, chosenFilename) = os.path.split(chosenLocalFile)
 
-print chosenLocalFile, ' is the local file that was randomly picked'
+print chosenFilename, 'is the local file that was randomly picked'
 
-print 'we will delete', chosenDistantFile, ' and replace with', chosenLocalFile
+# Deleting Distan File
+sftp.remove(filePath + '/' + chosenDistantFile)
+print chosenDistantFile, 'has been deleted'
+
+# Move new file to Distant Device
+sftp.put(chosenLocalFile, filePath + '/' + chosenFilename)
+print chosenFilename, 'has been transfered to your device'
+
+# End Script
+sftp.close()
+ssh.close()
+print 'See you soon bro'
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Install
+On your android device, download and install SSHDroid.
+That's all you need !
+
+Rename config-example.py into config.py and edit your connexion info
